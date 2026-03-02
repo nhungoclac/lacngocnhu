82f983123bbf40809b721f2475a5c77144f100d7
dangkimanh01 <anhdangkim962@gmail.com>
2026-03-03 05:49:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -212,3 +212,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+// <!--Start of Tawk.to Script-->
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/69a5dfcc2f01051c35610930/1jinv4eps";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
+// <!--End of Tawk.to Script-->
