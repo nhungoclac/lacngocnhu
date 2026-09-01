67fa42cabefd45def1eecf71517fd3e477ca1588
Lạc Ngọc Như <nhulacngoc@gmail.com>
2026-09-01 13:06:23 +0700

    Update portfolio content from Web Admin UI

---
+++ data.js
@@ -0,0 +1,499 @@
+// data.js - Dữ liệu tập trung cho Portfolio Lạc Ngọc Như
+window.PORTFOLIO_DATA = {
+  "profile": {
+    "name": "Lạc Ngọc Như",
+    "badgeTitle": "Marketing",
+    "slogan": "\"Lạc - thích truyền thông, yêu công nghệ\"",
+    "avatar": "photo/avt1.jpg",
+    "email": "nhulacngoc@gmail.com",
+    "location": "TP. Hồ Chí Minh",
+    "birthYear": "2004",
+    "cvLink": "https://drive.google.com/file/d/1CRm0hvvJd5fkQZBLlEl1SQJHV02AQYpo/view?usp=sharing",
+    "githubRepo": "nhungoclac/lacngocnhu",
+    "facebook": "https://www.facebook.com/lacnhu04",
+    "linkedin": "https://www.linkedin.com/in/nh%C6%B0-l%E1%BA%A1c-ng%E1%BB%8Dc-947009350/"
+  },
+  "education": [
+    {
+      "date": "10/2022 - 01/2026",
+      "title": "Cử nhân Thương mại điện tử",
+      "sub": "Trường ĐH Công nghệ Thông tin, Đại học Quốc gia TP.HCM",
+      "desc": "Tốt nghiệp sớm (3.5 năm) - Đạt loại Giỏi"
+    }
+  ],
+  "experience": [
+    {
+      "id": "exp_droppii",
+      "date": "05/2026 - 09/2026",
+      "title": "Công ty TNHH Dandelion (Droppii)",
+      "sub": "Thực tập sinh Content Marketing",
+      "desc": "Quản lý Fanpage, viết các bài truyền thông chiến dịch, sales event, ads, expo, thông báo; brief hình ảnh và viết kịch bản teaser.",
+      "tools": [
+        "Meta Business",
+        "Google Sheet",
+        "Viết Brief",
+        "Viết Script",
+        "Theo dõi chỉ số"
+      ],
+      "workSamples": [
+        {
+          "img": "photo/droppii.png",
+          "title": "Admin của Fanpage 99.000 người theo dõi",
+          "link": "https://www.facebook.com/DroppiiOfficialPage"
+        },
+        {
+          "img": "photo/droppiiplan.png",
+          "title": "Phối hợp cùng team thực hiện đúng timeline",
+          "link": "#"
+        },
+        {
+          "img": "photo/caption1.png",
+          "title": "Caption cho các bài viết Fanpage",
+          "link": "https://docs.google.com/presentation/d/1K7RlZSVTFdNkNoY6R1secS39HHu1qAiK-tkSgUdA9QI/edit?slide=id.g3f56686ff42_0_31#slide=id.g3f56686ff42_0_31"
+        },
+        {
+          "img": "photo/brief.png",
+          "title": "Brief hình ảnh truyền thông",
+          "link": "https://docs.google.com/presentation/d/1mdY6wRFrGQLF3L-yfg_WiHMVrCT5tmzoXIps_mCFH-Q/edit?slide=id.g3e97853934f_1_15#slide=id.g3e97853934f_1_15"
+        },
+        {
+          "img": "photo/scriptdroppii.png",
+          "title": "Viết script cho video",
+          "link": "https://docs.google.com/spreadsheets/d/1c_raLFtHbNed8MJ9hM-OudekqZJYaxpHlDQ1KGLz7GE/edit?gid=0#gid=0"
+        }
+      ]
+    },
+    {
+      "id": "exp_make",
+      "date": "2026",
+      "title": "Dự án cá nhân & tự học",
+      "sub": "Tự động hóa quy trình đăng bài với Make.com",
+      "desc": "Xây dựng các kịch bản tự động hóa cho quy trình marketing: RSS quét bài viết từ internet -> AI tự lên ý tưởng, học giọng văn con người, viết caption, tạo ảnh, đăng bài, lưu trữ thông tin vào Telegram Bot & Google Sheet.",
+      "tools": [
+        "Make.com",
+        "Google Sheet",
+        "Telegram Bot",
+        "Gemini API",
+        "Leonardo API"
+      ],
+      "workSamples": [
+        {
+          "img": "photo/make1.png",
+          "title": "Overview",
+          "link": "#"
+        },
+        {
+          "img": "photo/make2.png",
+          "title": "Google Sheet",
+          "link": "#"
+        },
+        {
+          "img": "photo/make3.png",
+          "title": "Google Sheet",
+          "link": "#"
+        },
+        {
+          "img": "photo/make4.png",
+          "title": "Leonardo API",
+          "link": "#"
+        },
+        {
+          "img": "photo/make5.png",
+          "title": "Google AI Studio",
+          "link": "#"
+        },
+        {
+          "img": "photo/make6.png",
+          "title": "Telegram Bot",
+          "link": "#"
+        },
+        {
+          "img": "photo/make7.png",
+          "title": "Bài đăng tự động",
+          "link": "#"
+        }
+      ]
+    },
+    {
+      "id": "exp_lucas",
+      "date": "07/2025 - 10/2025",
+      "title": "Công ty TNHH Thương mại Lucas",
+      "sub": "Thực tập sinh Marketing",
+      "desc": "Quản lý Fanpage, quay/chụp sản phẩm, viết kịch bản viral clip, chỉnh video bằng CapCut Pro, thiết kế hình ảnh bằng Canva",
+      "tools": [
+        "SEO (WordPress)",
+        "Meta Business",
+        "CapCut",
+        "Canva",
+        "Lập kế hoạch",
+        "Viết kịch bản",
+        "Quay/chụp"
+      ],
+      "workSamples": [
+        {
+          "img": "photo/lucas.png",
+          "title": "Admin của Fanpage 123.000 người theo dõi",
+          "link": "https://lucas.vn/author/nhulac/page/4"
+        },
+        {
+          "img": "photo/SEO.png",
+          "title": "Dùng WordPress viết & đăng bài SEO",
+          "link": "#"
+        },
+        {
+          "img": "photo/xdcc2.png",
+          "title": "Lập kế hoạch & thực thi chiến dịch Xoay Đi Chờ Chi",
+          "link": "https://docs.google.com/spreadsheets/d/1ZKpP-XwpTRJh86EiEFHs0XhB-CHrNXfi/edit?gid=1498389441"
+        },
+        {
+          "img": "photo/20102.png",
+          "title": "Lập kế hoạch & thực thi chiến dịch Tháng 10 dịu dàng - Trao nàng yêu thương",
+          "link": "https://www.facebook.com/lucascombomac"
+        },
+        {
+          "img": "photo/script1.png",
+          "title": "Viết kịch bản quay video (văn phòng, công ty, trending,...)",
+          "link": "https://docs.google.com/document/d/1Qme_qta9JGh-6RbhNFK_ikMQ7LJa3TyLENWVr_V7GSE/edit?tab=t.0"
+        }
+      ]
+    },
+    {
+      "id": "exp_thct",
+      "date": "10/2025 - 12/2025",
+      "title": "Đồ án Thương mại xã hội",
+      "sub": "Vị trí: Phó nhóm",
+      "desc": "Lập kế hoạch bài đăng Fanpage, phân công nhân sự, theo dõi deadline, thiết kế hình ảnh, hỗ trợ diễn xuất & quay video,...",
+      "tools": [
+        "Meta Business",
+        "CapCut",
+        "Canva",
+        "Lập kế hoạch",
+        "Quay video"
+      ],
+      "workSamples": [
+        {
+          "img": "photo/thct.png",
+          "title": "Quản lý Fanpage của Tạp hóa Content",
+          "link": "https://www.facebook.com/profile.php?id=61581694814856"
+        },
+        {
+          "img": "photo/thct1.png",
+          "title": "Kế hoạch truyền thông Fanpage",
+          "link": "https://docs.google.com/spreadsheets/d/11hDVcBBFUSz9cr09Bz_EY0ZAwz9vE-gbU1U3S9Yrgqo/edit?gid=1420815595#gid=1420815595"
+        },
+        {
+          "img": "photo/report1.png",
+          "title": "Overview của Fanpage",
+          "link": "#"
+        }
+      ]
+    },
+    {
+      "id": "exp_delifood",
+      "date": "10/2024 - 12/2024",
+      "title": "Đồ án Digital Marketing",
+      "sub": "Vị trí: Trưởng nhóm",
+      "desc": "Phân chia công việc, theo dõi tiến độ, lập plan chi tiết, thiết kế hình ảnh, quản lý Fanpage, tạo website bằng WordPress, tùy chỉnh 1 số giao diện bằng UX builder & CSS,... (số lượng thành viên: 4)",
+      "tools": [
+        "Meta Business",
+        "TikTok Analytics",
+        "WordPress",
+        "CSS",
+        "Figma",
+        "Canva",
+        "Lập kế hoạch"
+      ],
+      "workSamples": [
+        {
+          "img": "photo/delifood.png",
+          "title": "Quản lý Fanpage của Delifood",
+          "link": "https://www.facebook.com/delifoodvietnam"
+        },
+        {
+          "img": "photo/delifoodtiktok.png",
+          "title": "Quản lý kênh TikTok của Delifood",
+          "link": "https://www.tiktok.com/@delifoodvn?is_from_webapp=1&sender_device=pc"
+        },
+        {
+          "img": "photo/figma.png",
+          "title": "Thiết kế giao diện landing page trên Figma",
+          "link": "https://www.figma.com/design/bp8TIzDrGAPV26AvrmFNka/Landing-page?node-id=0-1&p=fc"
+        },
+        {
+          "img": "photo/css.png",
+          "title": "Thêm lead form vào website bằng CSS",
+          "link": "#"
+        },
+        {
+          "img": "photo/delifood1.png",
+          "title": "Kế hoạch chi tiết (brand marketing)",
+          "link": "https://docs.google.com/spreadsheets/d/1EGUwO55bduOgcLzJZj3gteLVLCtAs5K1W9p23G5uOaY/edit?gid=625374556#gid=625374556"
+        },
+        {
+          "img": "photo/doandelifood.png",
+          "title": "Tự đánh giá đồ án (GV yêu cầu)",
+          "link": "https://docs.google.com/document/d/1A8JeJhsUhVVWsnTgY09tDh70kTX-5PR_zLag70-UXGs/edit?tab=t.0"
+        }
+      ]
+    }
+  ],
+  "skills": [
+    {
+      "name": "SEO",
+      "icon": "fas fa-chart-line",
+      "colorClass": "icon-seo"
+    },
+    {
+      "name": "Meta Business",
+      "icon": "fab fa-facebook",
+      "colorClass": "icon-meta"
+    },
+    {
+      "name": "TikTok Analytics",
+      "icon": "fab fa-tiktok",
+      "colorClass": "icon-tiktok"
+    },
+    {
+      "name": "Figma",
+      "icon": "fab fa-figma",
+      "colorClass": "icon-figma"
+    },
+    {
+      "name": "WordPress",
+      "icon": "fab fa-wordpress",
+      "colorClass": "icon-wordpress"
+    },
+    {
+      "name": "Quay/Chụp",
+      "icon": "fas fa-camera",
+      "colorClass": "icon-camera"
+    },
+    {
+      "name": "CapCut",
+      "icon": "fas fa-scissors",
+      "colorClass": "icon-capcut"
+    },
+    {
+      "name": "Canva",
+      "icon": "fas fa-paint-brush",
+      "colorClass": "icon-canva"
+    },
+    {
+      "name": "HTML/CSS cơ bản",
+      "icon": "fab fa-html5",
+      "colorClass": "icon-html"
+    },
+    {
+      "name": "Viết kịch bản",
+      "icon": "fas fa-feather",
+      "colorClass": "icon-script"
+    },
+    {
+      "name": "Lập kế hoạch",
+      "icon": "fas fa-tasks",
+      "colorClass": "icon-plan"
+    },
+    {
+      "name": "Google Sheet",
+      "icon": "fas fa-table",
+      "colorClass": "icon-gsheet"
+    },
+    {
+      "name": "Make.com (Automation)",
+      "icon": "fas fa-robot",
+      "colorClass": "icon-make"
+    },
+    {
+      "name": "Tự học các công cụ",
+      "icon": "fas fa-book-open",
+      "colorClass": "icon-learn"
+    },
+    {
+      "name": "AI (ChatGPT, Gemini, Codex,...)",
+      "icon": "fas fa-brain",
+      "colorClass": "icon-ai"
+    },
+    {
+      "name": "Veo3 (Video AI)",
+      "icon": "fas fa-video",
+      "colorClass": "icon-veo"
+    }
+  ],
+  "videos": {
+    "highlight": "✨ Tự lên ý tưởng, viết kịch bản ✨",
+    "droppiiSectionTitle": "Droppii",
+    "droppiiReels": [
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1284679383544296%2F&show_text=false&width=476&t=0",
+        "caption": "Remind Ngày hội NutriBest Health (2.400 views)",
+        "views": "2.400"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1655116209118584%2F&show_text=false&width=476&t=0",
+        "caption": "Teaser sale 7/7 (97.000 views - Có ads)",
+        "views": "97.000"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1036814988458925%2F&show_text=false&width=476&t=0",
+        "caption": "Nút vàng 2 tỷ (1.800 views)",
+        "views": "1.800"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F624647343606622%2F&show_text=false&width=476&t=0",
+        "caption": "Teaser Expo (15.000 views)",
+        "views": "15.000"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1240166297379730%2F&show_text=false&width=476&t=0",
+        "caption": "Showcase NutriBest (1.900 views)",
+        "views": "1.900"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1253457599387063%2F&show_text=false&width=476&t=0",
+        "caption": "Remind livestream 7/7 (1.400 views)",
+        "views": "1.400"
+      }
+    ],
+    "lucasSectionTitle": "Cửa hàng Lucas (Phụ kiện Apple)",
+    "lucasReels": [
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1321034079427387%2F&show_text=false&width=476&t=0",
+        "caption": "Hội chợ phụ kiện siêu hời tại Lucas (1.500 views)",
+        "views": "1.500"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F807530514869877%2F&show_text=false&width=476&t=0",
+        "caption": "Cách khắc phục bút Apple Pencil bị ngắt kết nối (2.400 views)",
+        "views": "2.400"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F770381018933221%2F&show_text=false&width=476&t=0",
+        "caption": "Văn hóa công ty Lucas (1.100 views)",
+        "views": "1.100"
+      },
+      {
+        "url": "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1139474771092686%2F&show_text=false&width=476&t=0",
+        "caption": "Túi chống sốc Tomtoc bảo vệ MacBook (4.600 views)",
+        "views": "4.600"
+      }
+    ]
+  },
+  "campaigns": [
+    {
+      "title": "1. Thực tập sinh Content Marketing tại Droppii",
+      "date": "05/2026 - 09/2026",
+      "link": "https://docs.google.com/presentation/d/1-jYV12X3Xn9Xn8Xn7X6/edit?usp=sharing",
+      "linkText": "Xem chi tiết báo cáo chiến dịch",
+      "description": "Thực hiện quản lý nội dung Fanpage, triển khai chiến dịch truyền thông đa kênh cho các đợt Sale Event, Expo và thông báo ra mắt sản phẩm.",
+      "stats": [
+        {
+          "label": "Reach tổng chiến dịch",
+          "number": "1.2M+"
+        },
+        {
+          "label": "Lượt tương tác",
+          "number": "85K+"
+        },
+        {
+          "label": "Tăng trưởng Fanpage",
+          "number": "+12%"
+        }
+      ]
+    },
+    {
+      "title": "2. Chiến dịch Tháng 10 Dịu Dàng - Trao Nàng Yêu Thương (Lucas)",
+      "date": "10/2025",
+      "link": "https://www.facebook.com/lucascombomac",
+      "linkText": "Xem bài đăng chiến dịch",
+      "description": "Lên kế hoạch và thực thi chuỗi bài đăng truyền thông nhân ngày 20/10, thúc đẩy doanh số bán lẻ phụ kiện phụ nữ.",
+      "stats": [
+        {
+          "label": "Doanh thu tăng",
+          "number": "+35%"
+        },
+        {
+          "label": "Tương tác tự nhiên",
+          "number": "15.000+"
+        }
+      ]
+    }
+  ],
+  "designs": [
+    {
+      "title": "Thiết kế Poster & Banner Sản phẩm",
+      "tag": "Social Media Banner",
+      "images": [
+        "photo/bku.png",
+        "photo/bku2.png",
+        "photo/bku3.png",
+        "photo/bku4.jpg"
+      ]
+    },
+    {
+      "title": "Thiết kế Visual Content Truyền thông",
+      "tag": "Branding & Visual",
+      "images": [
+        "photo/bku5.jpg",
+        "photo/bku6.jpg",
+        "photo/bku7.jpg",
+        "photo/bku8.jpg"
+      ]
+    }
+  ],
+  "memes": [
+    {
+      "id": "meme_1",
+      "caption": "Meme dạng thả vote để giới thiệu sạc dự phòng WiWU",
+      "images": [
+        "photo/meme1.png",
+        "photo/memere.jpg"
+      ]
+    },
+    {
+      "id": "meme_2",
+      "caption": "Meme tương tác chăm sóc khách hàng & quà tặng",
+      "images": [
+        "photo/meme2.png",
+        "photo/memere2.jpg"
+      ]
+    },
+    {
+      "id": "meme_3",
+      "caption": "Meme sản phẩm phụ kiện Macbook",
+      "images": [
+        "photo/meme3.png",
+        "photo/memeca1.png"
+      ]
+    },
+    {
+      "id": "meme_4",
+      "caption": "Meme giải trí văn phòng",
+      "images": [
+        "photo/meme4.png",
+        "photo/memeca2.png"
+      ]
+    }
+  ],
+  "briefs": [
+    {
+      "title": "Brief hình ảnh chiến dịch Sale 7/7",
+      "date": "07/2026",
+      "metaImage": "photo/brief-77-meta.png",
+      "resultImages": [
+        "photo/brief-77-result.png",
+        "photo/brief-77-result01.png"
+      ],
+      "link": "https://docs.google.com/presentation/d/1mdY6wRFrGQLF3L-yfg_WiHMVrCT5tmzoXIps_mCFH-Q/edit"
+    },
+    {
+      "title": "Brief hình ảnh Brand Awareness NutriBest",
+      "date": "08/2026",
+      "metaImage": "photo/brief-brand-meta.png",
+      "resultImages": [
+        "photo/brief-brand-result.png",
+        "photo/brief-brand-result01.png"
+      ],
+      "link": "#"
+    }
+  ]
+};
